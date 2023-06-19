e9d3de7e3fe9cf6f7a8888097b3e3bfaef151c4d
Cyril Attie <cyril@attie.com>
2023-06-19 15:04:43 +0200

    permitir las cors desde almazenunir.es

--- app.js
+++ app.js
@@ -7,10 +7,11 @@ const apiRouter = require('./routes/api');
 
 const app = express();
 
-var whitelist = ['http://localhost:4200','http://almazenunir.es','http://www.almazenunir.es']
+var whitelist = ['http://localhost:4200','http://localhost:3000','http://almazenunir.es','http://www.almazenunir.es']
 var corsOptions = {
     origin: function (origin, callback) {
-        if (whitelist.indexOf(origin) !== -1) {
+        console.log(origin)
+        if (whitelist.indexOf(origin) !== -1 ||!origin) {
             callback(null, true)
         } else {
             callback(new Error('Not allowed by CORS'))
@@ -25,7 +26,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'dist/front_logistica/')));
+app.use(express.static(path.join(__dirname, 'front_logistica/dist/front_logistica')));
 
 app.use('/api', apiRouter);
 
@@ -35,7 +36,7 @@ app.use('/api', apiRouter);
 
 // Al front todo el resto 
 app.get('*', function (req, res) {
-    res.sendFile(path.resolve('dist/front_logistica/index.html'));
+    res.sendFile(path.resolve('front_logistica/dist/front_logistica/index.html'));
 });
 
 module.exports = app;
